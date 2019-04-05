4ee5f3b0a80180d47bafd4ca4ea1d8b165d0b1de
Ludwik1526 <mmichal1526@gmail.com>
2019-04-05 11:23:47 +0200

    Capturables movement script

--- AVR3/Assets/Scripts/CapturablesMovement.cs
+++ AVR3/Assets/Scripts/CapturablesMovement.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class CapturablesMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+    private float randomX;
+    private float randomZ;
+    private float randomY;
+    private Vector3 currentRandomPos;
+
+
+    void Start()
+    {
+        PickPosition();
+        randomX = Random.Range(-2f, 2f);
+        randomY = Random.Range(-2f, 2f);
+        randomZ = Random.Range(-2f, 2f);
+
+        switch (this.gameObject.tag)
+        {
+            case "Tag1": speed = 5;
+                break;
+            case "Tag2": speed = 10;
+                break;
+        }
+    }
+
+    void PickPosition()
+    {
+        currentRandomPos = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), Random.Range(-randomZ, randomZ);
+        StartCoroutine(MoveToRandomPos());
+
+    }
+
+    IEnumerator MoveToRandomPos()
+    {
+        float i = 0.0f;
+        float rate = 1.0f / speed;
+        Vector3 currentPos = transform.position;
+
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(currentPos, currendRandomPos, i);
+            yield return null;
+        }
+
+        float randomFloat = Random.Range(0.0f, 1.0f); // Create %50 chance to wait
+        if (randomFloat < 0.5f)
+            StartCoroutine(WaitForSomeTime());
+        else
+            PickPosition();
+    }
+
+    IEnumerator WaitForSomeTime()
+    {
+        yield return new WaitForSeconds(1);
+        PickPosition();
+    }
+
+
 }
