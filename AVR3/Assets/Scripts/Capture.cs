cc199b9eb0af3c41a2e6a0c1996c03e89596e76e
Smitchy <life.lime@gmail.com>
2019-04-08 14:38:47 +0200

    still trying to stop movement

---
+++ AVR3/Assets/Scripts/Capture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class Capture : MonoBehaviour, IInputClickHandler {
+
+    public delegate void ICapture();
+    public ICapture iCapture;
+
+    private bool captured;
+    private CapturablesMovement movement;
+
+    private void Start()
+    {
+        captured = false;
+        movement = GetComponent<CapturablesMovement>();
+        iCapture += MovementToggle;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        iCapture();
+    }
+
+    private void MovementToggle()
+    {
+        captured = !captured;
+        movement.enabled = captured;
+    }
+}
