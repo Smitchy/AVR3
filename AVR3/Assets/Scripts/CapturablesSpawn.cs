e422dd754862b14c41b4aad459fe0d3decbc2272
Ludwik1526 <mmichal1526@gmail.com>
2019-04-08 11:25:18 +0200

    Spawn

---
+++ AVR3/Assets/Scripts/CapturablesSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturablesSpawn : MonoBehaviour
+{
+
+    public int numberToSpawn;
+    public float spawningInterval;
+    public float spawningRadius;
+
+    private int numberSpawned;
+    private Vector3 spawnPosition;
+    private Vector3 cameraPos;
+
+    public GameObject[] capturables;
+
+    void Start ()
+    {
+        numberSpawned = 0;
+        cameraPos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+       
+        StartCoroutine(SpawningRoutine());
+    }
+	
+
+    void ChooseSpawnPosition()
+    {
+        spawnPosition = cameraPos + new Vector3(Random.Range(-spawningRadius, spawningRadius), Random.Range(-spawningRadius, spawningRadius), Random.Range(-spawningRadius, spawningRadius));
+    }
+
+    IEnumerator SpawningRoutine()
+    {
+        while (numberSpawned < numberToSpawn)
+        {
+            ChooseSpawnPosition();
+            Instantiate(capturables[Random.Range(0, 5)], spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(spawningInterval - numberSpawned/5.0f);
+        }
+    }
+}
