46d1c52d1eeae3c77646943d0db602cc4fb99169
Ludwik1526 <mmichal1526@gmail.com>
2019-04-08 13:45:52 +0200

    Assets for particles

---
+++ AVR3/Assets/Scripts/ChestStorage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestStorage : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
