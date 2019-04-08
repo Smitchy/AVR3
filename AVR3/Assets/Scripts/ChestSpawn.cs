5a5f84188fe112d4a2097cf07a32863512a90990
Ludwik1526 <mmichal1526@gmail.com>
2019-04-08 14:04:40 +0200

    Chest scripts v1

---
+++ AVR3/Assets/Scripts/ChestSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestSpawn : MonoBehaviour {
+
+    public GameObject chest;
+    public Transform cameraPos;
+
+	void Start () {
+        SpawnChest();
+    }
+	
+    void SpawnChest()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cameraPos.position + new Vector3(0, 0, 3), -Vector3.up, out hit))
+        {
+                Instantiate(chest, hit.point, Quaternion.identity);
+        }
+    }
+}
--- AVR3/Assets/Scripts/ChestStorage.cs
+++ AVR3/Assets/Scripts/ChestStorage.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestStorage : MonoBehaviour {
-    
-	void Start () {
-		
-	}
+public class ChestStorage : MonoBehaviour
+{
+
+    private GameObject chest;
+    private bool isChestAssigned = false;
+
+    public Animator anim;
+    public Animation openChest;
+    public Animation closeChest;
+
+	void Start ()
+    {
+    }
 	
 	void Update () {
-		
+
+        if (!isChestAssigned)
+        {
+            chest = GameObject.FindGameObjectWithTag("chest");
+            anim = chest.GetComponent<Animator>();
+            isChestAssigned = true;
+        }
+
 	}
 }
