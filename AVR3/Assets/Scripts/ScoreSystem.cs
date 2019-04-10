583431618529620f9f019acd2faba3872fe7e43b
Ludwik1526 <mmichal1526@gmail.com>
2019-04-10 20:09:53 +0200

    Game over screen + restart

--- AVR3/Assets/Scripts/ScoreSystem.cs
+++ AVR3/Assets/Scripts/ScoreSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
@@ -18,6 +19,10 @@ public class ScoreSystem : MonoBehaviour
     public Text score, timer;
     private int time;
 
+    private Color defaultColor;
+    public GameObject gameOver;
+    public Button restartB;
+
     void Awake() {
         if(instance != null && instance != this)
         {
@@ -26,44 +31,78 @@ public class ScoreSystem : MonoBehaviour
         }
         else
             instance = this;
-            
-    }
+        }
+
     void Start()
     {
+        Time.timeScale = 1f;
         numberOfCapturables = 0;
         numberCaught = 0;
         time = 30;
+        StartCoroutine(TimerRoutine());
+        defaultColor = timer.color;
+
+        gameOver.SetActive(false);
+        restartB.gameObject.SetActive(false);
+
+        restartB.onClick.AddListener(Restart);
     }
 
-    IEnumerable TimerRoutine()
+    void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator TimerRoutine()
     {
         while(time > 0)
         {
             time--;
             yield return new WaitForSeconds(1);
         }
-        YouLost();
     }
 
     void Update()
     {
 
         score.text = "Caught: "+numberCaught;
+        
+        if ((time - (time / 60) * 60) < 10)
+        {
+            timer.text = "Time left: 0" + (time / 60) + ":0" + (time - (time / 60) * 60);
+        }
+        else
+        {
+            timer.text = "Time left: 0" + (time / 60) + ":" + (time - (time / 60) * 60);
+        }
 
-        if (numberOfCapturables >= numberAllowed)
+        if (time < 10)
+        {
+            timer.color = Color.red;
+            score.color=Color.red;
+        }
+        else
+        {
+            timer.color = defaultColor;
+            score.color = defaultColor;
+        }
+
+        if (time == 0)
         {
             YouLost();
         }
-    }
 
+    }
 
     void YouLost()
     {
-        Debug.Log("You Lost");
+        gameOver.SetActive(true);
+        restartB.gameObject.SetActive(true);
+        Time.timeScale = 0f;
     }
 
     public void Catch(){
         numberCaught++;
-        time += 5;
+        time += 6;
     }
 }
