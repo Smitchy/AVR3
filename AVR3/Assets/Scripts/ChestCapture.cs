bd56804b7b78612df28f66d1f52c16384306b79f
Ludwik1526 <mmichal1526@gmail.com>
2019-04-10 20:26:13 +0200

    Particle system added

--- AVR3/Assets/Scripts/ChestCapture.cs
+++ AVR3/Assets/Scripts/ChestCapture.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ChestCapture : MonoBehaviour {
 
 	public Animator anim;
+    public GameObject[] particles;
 
 	private void OnTriggerEnter(Collider other) {
 		ScoreSystem.Instance.Catch();
 		Destroy(other.gameObject);
-	}
+        Instantiate(particles[Random.Range(0, particles.Length)], transform.position, Quaternion.identity);
+    }
 }
