cc47ab4699294679ced233ef13b230e17e643e6b
Ludwik1526 <mmichal1526@gmail.com>
2019-04-10 20:33:01 +0200

    Particles fix + audio

--- AVR3/Assets/Scripts/ChestCapture.cs
+++ AVR3/Assets/Scripts/ChestCapture.cs
@@ -6,10 +6,13 @@ public class ChestCapture : MonoBehaviour {
 
 	public Animator anim;
     public GameObject[] particles;
-
+    public AudioSource source;
+    public AudioClip fireworks;
+    
 	private void OnTriggerEnter(Collider other) {
 		ScoreSystem.Instance.Catch();
 		Destroy(other.gameObject);
         Instantiate(particles[Random.Range(0, particles.Length)], transform.position, Quaternion.identity);
+        source.PlayOneShot(fireworks);
     }
 }
